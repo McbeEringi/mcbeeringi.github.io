79ef996a728cc418e78973f732f7bc33c639232f
McbeEringi <46889548+McbeEringi@users.noreply.github.com>
2020-02-14 20:22:37 +0900

    Update hf.js

--- hf.js
+++ hf.js
@@ -1,21 +1,22 @@
 document.getElementById("H").innerHTML = "\
-	<a href=\"https://mcbeeringi.github.io\" class=\"left\">\
-		<img src=\"https://mcbeeringi.github.io/img/6x6.png\" width=\"32\" height=\"32\" alt=\"logo\">\
+	<a href='https://mcbeeringi.github.io' class='left'>\
+		<img src='https://mcbeeringi.github.io/img/6x6.png' width='32' height='32' alt='logo'>\
 		<h3>McbeEringi</h3>\
 	</a>\
-	<nav class=\"right\" style=\"margin-right:1%;flex-grow: 1;\">\
-		<a href=\"https://mcbeeringi.github.io/esbe-2g\">ESBE 2G</a>\
-		<a href=\"https://mcbeeringi.github.io/java-jajp\">java ja_JP</a>\
-		<a href=\"https://mcbeeringi.github.io/others\">Others</a>\
-		<a href=\"https://mcbeeringi.github.io/worlds\">Worlds</a>\
+	<nav class='right' style='margin-right:1%;flex-grow: 1;'>\
+		<a href='https://mcbeeringi.github.io/esbe-2g'>ESBE 2G</a>\
+		<a href='https://mcbeeringi.github.io/java-jajp'>java ja_JP</a>\
+		<a href='https://mcbeeringi.github.io/others'>Others</a>\
+		<a href='https://mcbeeringi.github.io/worlds'>Worlds</a>\
 	</nav>\
 ";
 
 document.getElementById("F").innerHTML = "\
-	<div class=\"text\">\
-		<p>© 2018~ <a href=\"https://twitter.com/mcbeeringi\">@McbeEringi</a>.｡:+*</p>\
+	<div class='text'>\
+		<p>© 2018~ <a href='https://twitter.com/mcbeeringi'>@McbeEringi</a>.｡:+*</p>\
 		<p>\
-			<a href=\"https://www.youtube.com/channel/UC7KFkUaWgpmdHViToV1wCAA\">YouTube</a>\
+			<a href='https://www.youtube.com/channel/UC7KFkUaWgpmdHViToV1wCAA'>YouTube</a>\
+			<a href='https://github.com/mcbeeringi'>GitHub</a>\
 		</p>\
 	</div>\
 ";
