f79b66123ec7b127cadd8e987b771f7daad4a597
McbeEringi <46889548+McbeEringi@users.noreply.github.com>
2020-06-02 18:17:03 +0900

    -

---
+++ apps/sky/sky_sw.js
@@ -0,0 +1,4 @@
+//https://qiita.com/umamichi/items/0e2b4b1c578e7335ba20
+self.addEventListener('install', function(e){console.log('[ServiceWorker] Install');});
+self.addEventListener('activate', function(e){console.log('[ServiceWorker] Activate');});
+//self.addEventListener('fetch', function(e){});
