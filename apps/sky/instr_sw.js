d10f1faa88b92842da967e7ebf4f6fa8d4db9f7b
McbeEringi <46889548+McbeEringi@users.noreply.github.com>
2020-06-11 05:24:29 +0900

    -

--- apps/sky/instr_sw.js
+++ apps/sky/instr_sw.js
@@ -1,4 +1,25 @@
-//https://qiita.com/umamichi/items/0e2b4b1c578e7335ba20
-self.addEventListener('install', function(e){console.log('[ServiceWorker] Install');});
+//https://qiita.com/masanarih0ri/items/0845f312cff5c8d0ec60
+const STATIC_DATA = [
+	'instr.html',
+	'instr.png',
+	'sky_style.js',
+	'sky_style.css'
+];
+
+self.addEventListener('install', function(e) {
+	e.waitUntil(
+		caches.open('cache_v1').then(function(cache) {
+			return cache.addAll(STATIC_DATA);
+		})
+	);
+	console.log('[ServiceWorker] Install');
+});
 self.addEventListener('activate', function(e){console.log('[ServiceWorker] Activate');});
-//self.addEventListener('fetch', function(e){});
+self.addEventListener('fetch', function(e) {
+	console.log('[ServiceWorker] Fetch'+e.request.url);
+	e.respondWith(
+		caches.match(e.request).then(function(response) {
+			return response || fetch(e.request);
+		})
+	);
+});
