a93f62f3d5c9dda7c4a8f4220daddb01db66a094
McbeEringi <46889548+McbeEringi@users.noreply.github.com>
2020-06-28 18:43:58 +0900

    -

--- apps/sky/instr_sw.js
+++ apps/sky/instr_sw.js
@@ -1,13 +1,15 @@
 //https://qiita.com/masanarih0ri/items/0845f312cff5c8d0ec60
 const STATIC_DATA = [
 	'instr.html',
+	'https://cdnjs.cloudflare.com/ajax/libs/tone/14.5.45/Tone.js',
+	'https://cdn.jsdelivr.net/npm/js-cookie@2/src/js.cookie.min.js',
 	'instr.png',
 	'sky_style.js',
 	'sky_style.css',
-	'/audio/harp/d4.mp3',
-	'/audio/harp/g4.mp3',
-	'/audio/harp/c5.mp3',
-	'/audio/harp/fs5.mp3'
+	'/audio/harp/a2.mp3',
+	'/audio/harp/a3.mp3',
+	'/audio/harp/ds2.mp3',
+	'/audio/harp/ds3.mp3'
 ];
 
 self.addEventListener('install', function(e) {
