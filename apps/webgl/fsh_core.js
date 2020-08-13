4d978a54e7c50071720cbb09cd0072027cbe0c8e
McbeEringi <46889548+McbeEringi@users.noreply.github.com>
2020-08-14 01:10:20 +0900

    -

--- apps/webgl/fsh_core.js
+++ apps/webgl/fsh_core.js
@@ -104,11 +104,13 @@ function tex(i){
 		gl.bindTexture(gl.TEXTURE_2D, tex);//バインドセット
 		gl.pixelStorei(gl.UNPACK_FLIP_Y_WEBGL, true);//y反転
 		gl.texImage2D(gl.TEXTURE_2D, 0, gl.RGBA, gl.RGBA, gl.UNSIGNED_BYTE, img);//読み込み
-		gl.generateMipmap(gl.TEXTURE_2D);//ミップマップ生成
-		gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_MAG_FILTER, gl.LINEAR);//拡大縮小
-		gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_MIN_FILTER, gl.LINEAR);
-		gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_WRAP_S, gl.CLAMP_TO_EDGE);//端数処理
-		gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_WRAP_T, gl.CLAMP_TO_EDGE);
+		try{gl.generateMipmap(gl.TEXTURE_2D);}//ミップマップ生成
+		catch(){
+			gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_MAG_FILTER, gl.LINEAR);//拡大縮小
+			gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_MIN_FILTER, gl.LINEAR);
+			gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_WRAP_S, gl.CLAMP_TO_EDGE);//端数処理
+			gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_WRAP_T, gl.CLAMP_TO_EDGE);
+		}
 		gl.bindTexture(gl.TEXTURE_2D, null);//バインド解除
 		imgres[i] = [img.naturalWidth,img.naturalHeight];
 		document.getElementById(i+"reslog").textContent=imgres[i].join(", ");
