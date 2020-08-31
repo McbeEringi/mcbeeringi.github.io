9d943da168ce75a9cea7be62a405fbe11f4a31ea
McbeEringi <46889548+McbeEringi@users.noreply.github.com>
2020-08-31 22:02:37 +0900

    -

--- main.js
+++ main.js
@@ -1,23 +1,34 @@
-document.write('<link href="https://fonts.googleapis.com/css2?family=Lato:wght@300&family=M+PLUS+Rounded+1c&display=swap" rel="stylesheet">\n\
-<link href="https://mcbeeringi.github.io/src/main.css" rel="stylesheet">');
+document.write(`<link href="https://fonts.googleapis.com/css2?family=Lato:wght@300&family=M+PLUS+Rounded+1c&display=swap" rel="stylesheet">
+<link href="src/main.css" rel="stylesheet">`);
 var header = document.createElement('header');
-header.innerHTML = `
+header.insertAdjacentHTML('beforeend',`
 	<a href="https://mcbeeringi.github.io" class="left">
 		<img src="https://mcbeeringi.github.io/img/6x6.png" width="32" height="32" alt="logo">
 		<h3>McbeEringi</h3>
 	</a>
-	<nav class="right" style="margin-right:4px;flex-grow:1;">
-		<a href="https://mcbeeringi.github.io/esbe-2g">ESBE 2G</a>
-		<a href="https://mcbeeringi.github.io/java-jajp">java ja_JP</a>
-		<a href="https://mcbeeringi.github.io/sky">Sky</a>
-		<a href="https://mcbeeringi.github.io/apps">Apps</a>
-		<lang onClick="window.open('https://translate.google.com/translate?sl=ja&tl=en&u='+location.href)"></lang>
+	<nav>
+		<div id="hnavd">
+			<a href="https://mcbeeringi.github.io/esbe-2g">ESBE 2G</a>
+			<a href="https://mcbeeringi.github.io/java-jajp">java ja_JP</a>
+			<a href="https://mcbeeringi.github.io/sky">Sky</a>
+			<a href="https://mcbeeringi.github.io/apps">Apps</a>
+			<lang onClick="window.open('https://translate.google.com/translate?sl=ja&tl=en&u='+location.href)"></lang>
+		</div>
+		<input type="checkbox" id="hnavmcb">
+		<div id="hnavm">
+			<a href="https://mcbeeringi.github.io/esbe-2g">ESBE 2G</a>
+			<a href="https://mcbeeringi.github.io/java-jajp">java ja_JP</a>
+			<a href="https://mcbeeringi.github.io/sky">Sky</a>
+			<a href="https://mcbeeringi.github.io/apps">Apps</a>
+			<lang onClick="window.open('https://translate.google.com/translate?sl=ja&tl=en&u='+location.href)"></lang>
+		</div>
+		<label id="hnavml" for="hnavmcb"></label>
 	</nav>
-`;
+`);
 //header.setAttribute('id','H');
 document.body.appendChild(header);
 var footer = document.createElement('footer');
-footer.innerHTML=`
+footer.insertAdjacentHTML('beforeend',`
 	<div class="text">
 		<p>© 2018~ <a href="https://twitter.com/mcbeeringi">@McbeEringi</a>.｡:+*</p>
 		<p>
@@ -25,6 +36,6 @@ footer.innerHTML=`
 			<a href="https://github.com/mcbeeringi">GitHub</a>
 		</p>
 	</div>
-`;
+`);
 //footer.setAttribute('id','F');
 window.onload=()=>document.body.appendChild(footer);
