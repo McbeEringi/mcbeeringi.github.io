f5493bd9c76f4fbb76b9106a0bb325d00a9fd7bb
McbeEringi <46889548+McbeEringi@users.noreply.github.com>
2020-09-12 00:07:36 +0900

    -

--- src/style.js
+++ src/style.js
@@ -1,8 +1,3 @@
-document.write(`<link href="https://fonts.googleapis.com/css2?family=Lato:wght@300&family=M+PLUS+Rounded+1c&display=swap" rel="stylesheet">
-<style>
-</style>
-<link href="https://mcbeeringi.github.io/src/style.css" rel="stylesheet">
-`);
 var header = document.createElement('header');
 header.insertAdjacentHTML('beforeend',`
 	<a href="https://mcbeeringi.github.io">McbeEringi</a>
@@ -17,8 +12,7 @@ header.insertAdjacentHTML('beforeend',`
 		<input type="checkbox" id="hnavmcb"><div id="hnavm"></div><label id="hnavml" for="hnavmcb"></label>
 	</nav>
 `);
-//header.setAttribute('id','H');
-document.body.appendChild(header);
+document.body.insertBefore(header,document.body.firstChild);
 hnavm.insertAdjacentHTML('beforeend',hnavd.innerHTML);
 var footer = document.createElement('footer');
 footer.insertAdjacentHTML('beforeend',`
@@ -27,8 +21,10 @@ footer.insertAdjacentHTML('beforeend',`
 	<a href="https://www.youtube.com/channel/UC7KFkUaWgpmdHViToV1wCAA">YouTube</a>
 	<a href="https://github.com/mcbeeringi">GitHub</a>
 `);
-//footer.setAttribute('id','F');
-document.addEventListener('readystatechange',x=>{if(x.target.readyState=='interactive'){
-	document.body.appendChild(footer);
-	document.querySelectorAll('a').forEach(e=>{if(e.ontouchstart==undefined)e.ontouchstart=()=>{};});
-}},false);
+document.body.appendChild(footer);
+document.querySelectorAll('a').forEach(e=>{if(e.ontouchstart==undefined)e.ontouchstart=()=>{};});
+document.body.insertAdjacentHTML('afterbegin',`<link href="https://fonts.googleapis.com/css2?family=Lato:wght@300&family=M+PLUS+Rounded+1c&display=swap" rel="stylesheet">
+<style>
+</style>
+<link href="https://mcbeeringi.github.io/src/style.css" rel="stylesheet">
+`);
