3ced438859c0dcd4ca2cbd04d26dff5285193fbf
McbeEringi <46889548+McbeEringi@users.noreply.github.com>
2020-09-11 19:57:53 +0900

    -

---
+++ src/style.js
@@ -0,0 +1,34 @@
+document.write(`<link href="https://fonts.googleapis.com/css2?family=Lato:wght@300&family=M+PLUS+Rounded+1c&display=swap" rel="stylesheet">
+<style>
+</style>
+<link href="https://mcbeeringi.github.io/src/style.css" rel="stylesheet">
+`);
+var header = document.createElement('header');
+header.insertAdjacentHTML('beforeend',`
+	<a href="https://mcbeeringi.github.io">McbeEringi</a>
+	<nav>
+		<div id="hnavd">
+			<a href="https://mcbeeringi.github.io/esbe-2g">ESBE 2G</a>
+			<a href="https://mcbeeringi.github.io/java-jajp">java ja_JP</a>
+			<a href="https://mcbeeringi.github.io/sky">Sky</a>
+			<a href="https://mcbeeringi.github.io/apps">Apps</a>
+			<lang onClick="window.open('https://translate.google.com/translate?sl=ja&tl=en&u='+location.href)"></lang>
+		</div>
+		<input type="checkbox" id="hnavmcb"><div id="hnavm"></div><label id="hnavml" for="hnavmcb"></label>
+	</nav>
+`);
+//header.setAttribute('id','H');
+document.body.appendChild(header);
+hnavm.insertAdjacentHTML('beforeend',hnavd.innerHTML);
+var footer = document.createElement('footer');
+footer.insertAdjacentHTML('beforeend',`
+	<div class="text">
+		<p>© 2018~ <a href="https://twitter.com/mcbeeringi">@McbeEringi</a>.｡:+*</p>
+		<p>
+			<a href="https://www.youtube.com/channel/UC7KFkUaWgpmdHViToV1wCAA">YouTube</a>
+			<a href="https://github.com/mcbeeringi">GitHub</a>
+		</p>
+	</div>
+`);
+//footer.setAttribute('id','F');
+window.onload=()=>document.body.appendChild(footer);
