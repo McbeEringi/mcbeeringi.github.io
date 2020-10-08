8d8ce193adb7899dd7e0d4f19f15701b995bdee9
McbeEringi <46889548+McbeEringi@users.noreply.github.com>
2020-10-08 12:54:31 +0900

    -

--- apps/mc/poly.js
+++ apps/mc/poly.js
@@ -1,8 +1,10 @@
 const fx = str=>str.replace(/\n/g,"").split(" ").map(x=>x.replace(/0+$/g,"")).slice(1).map(x=>Number(x));
-const time = ()=>{var d=new Date();return `${('0'+d.getHours()).slice(-2)}:${('0'+d.getMinutes()).slice(-2)}:${('0'+(d.getSeconds()+d.getMilliseconds()/1e3).toFixed(3)).slice(-6)}`;}
-function core(s,posf,nf,uvf,i,cffx) {
+const time = ()=>{var d=new Date();return `${('0'+d.getHours()).slice(-2)}:${('0'+d.getMinutes()).slice(-2)}:${('0'+(d.getSeconds()+d.getMilliseconds()/1e3).toFixed(3)).slice(-6)}`;};
+var m='';
+function core(s,posf,nf,uvf,i,cffx){
 	console.log("core()");
 	//log.textContent += time()+"[info] started…\n";
+	var tmpm = s.match(/^usemtl\s.+$/gm);if(tmpm)m = tmpm.filter((x,i,a)=>a.indexOf(x)==i).map(x=>x.replace(/\n/g,"").replace(/^usemtl\s/,""));
 	var o = s.match(/^o\s.+$/m);
 		if(o){o = o[0].replace(/\n/g,"").replace(/^o\s/,"");}
 		else{self.postMessage(time()+"[info] o not found\n");o="poly"+i;}
@@ -19,17 +21,17 @@ function core(s,posf,nf,uvf,i,cffx) {
 		uv = uv.map(vt=>eval(cffx[2]));
 	var pol = s.match(/^f\s.+$/gm);
 		if(pol)pol = pol.map(str=>{str=str.replace(/\n/g,"").split(" ").map(x=>{x=x.split("/").map(x=>x?x-1:x);return [x[0]-posf,x[2]?x[2]-nf:0,x[1]?x[1]-uvf:0]}).slice(1);return [str[0],str[1],str[2],str[str[3]?3:0]]});
-		else{self.postMessage(time()+"[info] f not found… '"+o+"' skipped\n");return ["",pos.length,n.length,uv.length];}
+		else{self.postMessage(`${time()}[info] f not found… '${o}' skipped\n`);return ["",pos.length,n.length,uv.length];}
 	console.log(o,pos,uv,n,pol);
-	s =
-		'{"name":"'+o+'","pivot":[0,0,0]'+
+	s = `{"name":"${o}","pivot":[0,0,0],"poly_mesh":{"normalized_uvs":true,"positions":${JSON.stringify(pos)},"normals":${JSON.stringify(n)},"uvs":${JSON.stringify(uv)},"polys":${JSON.stringify(pol)}}},\n`;
+		/*'{"name":"'+o+'","pivot":[0,0,0]'+
 		',"poly_mesh":{"normalized_uvs":true,"positions":'+JSON.stringify(pos)+
 		',"normals":'+JSON.stringify(n)+',"uvs":'+JSON.stringify(uv)+
-		',"polys":'+JSON.stringify(pol)+'}},';
-	self.postMessage(time()+"[info] '"+o+"' done\n");
+		',"polys":'+JSON.stringify(pol)+'}},';*/
+	self.postMessage(`${time()}[info] '${o}' done	material:[${m}]\n`);
 	return [s,pos.length,n.length,uv.length];
 }
-function main(data) {
+function main(data){
 	console.log("main()");
 	self.postMessage(time()+"[info] all convertions started\n");
 	var posf=0,nf=0,uvf=0,diffuse="",tmp;
@@ -38,20 +40,20 @@ function main(data) {
 	if(s.match(/^o\s.+$/m))s = ("o\n#\n"+s).split(/\n(?=o)/).slice(1);
 	else s=[s];
 	console.log(s);
-	for(var i=0; i<s.length; i++) {
+	for(var i=0;i<s.length;i++){
 		tmp = core(s[i],posf,nf,uvf,i,data.cffx);
 		if(tmp){diffuse+=tmp[0]; posf+=tmp[1]; nf+=tmp[2]; uvf+=tmp[3];}
 		else {return["error"];}
 	}
-	diffuse = diffuse.slice(0,-1);
+	diffuse = diffuse.slice(0,-2);
 	if(frm){
 		o = frm;
-		diffuse = '{"format_version":"1.8.0","geometry.'+o+'":{"bones":['+diffuse+']}}';
+		diffuse = `{"format_version":"1.8.0","geometry.${o}":{"bones":[\n${diffuse}]}}`;//'{"format_version":"1.8.0","geometry.'+o+'":{"bones":['+diffuse+']}}';
 	}
 	self.postMessage(time()+"[info] all convertions completed");
 	return [o,diffuse];
 }
-self.addEventListener('message', (m)=>{
-	console.log(m.data);
-	self.postMessage({"diffuse":main(m.data)});
+self.addEventListener('message',msg=>{
+	console.log(msg.data);
+	self.postMessage({"diffuse":main(msg.data)});
 });
