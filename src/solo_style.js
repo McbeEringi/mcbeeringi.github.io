0393620202201f71169a8984d460d76bc14c7098
McbeEringi <46889548+McbeEringi@users.noreply.github.com>
2020-11-02 16:49:04 +0900

    Create solo_style.js

---
+++ src/solo_style.js
@@ -0,0 +1,19 @@
+document.body.insertAdjacentHTML('beforeend',`
+	<link rel="stylesheet" href="https://unpkg.com/swiper/swiper-bundle.min.css">
+	<style>
+		.swiper-container{width:90%;height:auto;}.swiper-container img{width:100%;}
+		.box{display:flex;flex-wrap:wrap;}.box>*{flex-grow:1;width:300px;}
+	</style>
+`)
+fetch('https://unpkg.com/swiper/swiper-bundle.min.js').then(r=>r.text()).then(t=>{
+	eval(t);
+	new Swiper('.swiper-container',{
+		slidesPerView:1,spaceBetween:0,loop:true,centeredSlides:true,
+		breakpoints:{
+			600:{slidesPerView:2,spaceBetween:30}
+		},
+		autoplay:{delay:2700,disableOnInteraction:false,},
+		pagination:{el:'.swiper-pagination',clickable:true},
+		navigation:{nextEl:'.swiper-button-next',prevEl:'.swiper-button-prev'}
+	});
+});
