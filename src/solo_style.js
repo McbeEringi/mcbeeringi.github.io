15592689012480b9c9bae497f04ea12b08bb3a84
McbeEringi <46889548+McbeEringi@users.noreply.github.com>
2020-11-03 13:24:11 +0900

    -

--- src/solo_style.js
+++ src/solo_style.js
@@ -1,19 +1,22 @@
+fetch('https://mcbeeringi.github.io/src/style.js').then(r=>r.text()).then(t=>eval(t));
 document.body.insertAdjacentHTML('beforeend',`
 	<link rel="stylesheet" href="https://unpkg.com/swiper/swiper-bundle.min.css">
 	<style>
 		.swiper-container{width:90%;height:auto;}.swiper-container img{width:100%;}
 		.box{display:flex;flex-wrap:wrap;}.box>*{flex-grow:1;width:300px;}
 	</style>
-`)
-fetch('https://unpkg.com/swiper/swiper-bundle.min.js').then(r=>r.text()).then(t=>{
-	eval(t);
-	new Swiper('.swiper-container',{
-		slidesPerView:1,spaceBetween:0,loop:true,centeredSlides:true,
-		breakpoints:{
-			600:{slidesPerView:2,spaceBetween:30}
-		},
-		autoplay:{delay:2700,disableOnInteraction:false,},
-		pagination:{el:'.swiper-pagination',clickable:true},
-		navigation:{nextEl:'.swiper-button-next',prevEl:'.swiper-button-prev'}
+`);
+window.addEventListener('DOMContentLoaded',()=>{
+	fetch('https://unpkg.com/swiper/swiper-bundle.min.js').then(r=>r.text()).then(t=>{
+		eval(t);
+		new Swiper('.swiper-container',{
+			slidesPerView:1,spaceBetween:0,loop:true,centeredSlides:true,
+			breakpoints:{
+				600:{slidesPerView:2,spaceBetween:30}
+			},
+			autoplay:{delay:2700,disableOnInteraction:false,},
+			pagination:{el:'.swiper-pagination',clickable:true},
+			navigation:{nextEl:'.swiper-button-next',prevEl:'.swiper-button-prev'}
+		});
 	});
 });
--- src/style.js
+++ src/style.js
@@ -14,18 +14,19 @@ header.insertAdjacentHTML('beforeend',`
 `);
 document.body.insertBefore(header,document.body.firstChild);
 hnavm.insertAdjacentHTML('beforeend',hnavd.innerHTML);
-var footer = document.createElement('footer');
-footer.insertAdjacentHTML('beforeend',`
-	© 2018~ @McbeEringi.｡:+*<br><br>
-	<a href="https://twitter.com/mcbeeringi">Twitter</a>
-	<a href="https://www.youtube.com/channel/UC7KFkUaWgpmdHViToV1wCAA">YouTube</a>
-	<a href="https://github.com/mcbeeringi">GitHub</a>
-`);
+window.addEventListener('DOMContentLoaded',()=>{
+	var footer = document.createElement('footer');
+	footer.insertAdjacentHTML('beforeend',`
+		© 2018~ @McbeEringi.｡:+*<br><br>
+		<a href="https://twitter.com/mcbeeringi">Twitter</a>
+		<a href="https://www.youtube.com/channel/UC7KFkUaWgpmdHViToV1wCAA">YouTube</a>
+		<a href="https://github.com/mcbeeringi">GitHub</a>
+	`);
+});
 document.body.appendChild(footer);
 document.querySelectorAll('a').forEach(e=>{if(e.ontouchstart==undefined)e.ontouchstart=()=>{};});
 document.querySelectorAll('.stuff img,.pad img').forEach(e=>e.width="512px");
 document.body.insertAdjacentHTML('afterbegin',`<link href="https://fonts.googleapis.com/css2?family=Lato:wght@300&family=M+PLUS+Rounded+1c&display=swap" rel="stylesheet">
-<style>
-</style>
+<style></style>
 <link href="https://mcbeeringi.github.io/src/style.css" rel="stylesheet">
 `);
