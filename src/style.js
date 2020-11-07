8f3ac5262c9f15c7b32564d6e77f40621af51c63
McbeEringi <46889548+McbeEringi@users.noreply.github.com>
2020-11-07 14:34:47 +0900

    -

--- src/style.js
+++ src/style.js
@@ -7,7 +7,7 @@ header.insertAdjacentHTML('beforeend',`
 			<a href="https://mcbeeringi.github.io/java-jajp">java ja_JP</a>
 			<a href="https://mcbeeringi.github.io/sky">Sky</a>
 			<a href="https://mcbeeringi.github.io/apps">Apps</a>
-			<a href=""><script>document.currentScript.parentElement.href='https://translate.google.com/translate?sl=ja&tl=en&u='+location.href;</script><lang></lang></a>
+			<a class="lang"></a>
 		</div>
 		<input type="checkbox" id="hnavmcb"><div id="hnavm"></div><label id="hnavml" for="hnavmcb"></label>
 	</nav>
@@ -23,7 +23,8 @@ const setstyle=()=>{
 		<a href="https://github.com/mcbeeringi">GitHub</a>
 	`);
 	document.body.appendChild(footer);
-	document.querySelectorAll('a').forEach(e=>{if(e.ontouchstart==undefined)e.ontouchstart=()=
+	document.querySelectorAll('.lang').forEach(e=>{e.href='https://translate.google.com/translate?sl=ja&tl=en&u='+location.href;console.log(e)});
+	document.querySelectorAll('a').forEach(e=>{if(e.ontouchstart==undefined)e.ontouchstart=()=>{};});
 	document.querySelectorAll('.stuff img,.pad img').forEach(e=>e.width="512px");
 };
 if(document.readyState=='loading')window.addEventListener('DOMContentLoaded',setstyle);else setstyle();
