1e1d3965871cce4d3d11add1045a30642d5ca829
McbeEringi <46889548+McbeEringi@users.noreply.github.com>
2020-12-07 21:43:29 +0900

    -

--- apps/webgl/fsh_core.js
+++ apps/webgl/fsh_core.js
@@ -1,53 +1,16 @@
-var comp_timer,save_timer;
-fsh_e.on("input",()=>{
-	clearTimeout(comp_timer);comp_timer=setTimeout(compile,500);
-	clearTimeout(save_timer);comp_timer=setTimeout(save,wait_.value,1);
-});
+addEventListener('load',()=>{
+	var comp_timer,save_timer;
+	fsh_e.on("input",()=>{
+		console.log('inp');
+		clearTimeout(comp_timer);comp_timer=setTimeout(compile,500);
+		clearTimeout(save_timer);comp_timer=setTimeout(save,wait_.value,1);
+	});
+})
 var imgres=[],dftflag,curdat,curname;
 diffuse.addEventListener('touchstart',()=>{dftflag=true;diffuse.src=canvas.toDataURL();},{passive:true})
 diffuse.onmousedown=()=>{if(dftflag)dftflag=false;else diffuse.src=canvas.toDataURL();}
 const llog=x=>log.insertAdjacentHTML('beforeend',x),
-copy=x=>{navigator.clipboard.writeText(x).catch(e=>console.log(e));return x;},
-thumb=()=>{};
-
-const save=s=>{
-	curdat={
-		fsh:fsh_e.getValue(),
-		size:[w_.value,h_.value],
-		fps:fps_.value,
-		thumb:canvas.toDataURL()
-	}
-	localStorage.curdat=JSON.stringify(curdat);
-	if(s)llog("\n<span style='color:#48f;'>auto saved.</span>");else console.log("saved");
-},
-expurl=()=>'mcbeeringi.github.io/apps/webgl/fsh.html#'+encodeURIComponent(JSON.stringify(curdat)).replace(/\(/g,"%28").replace(/\)/g,"%29"),
-inpurl=()=>{
-	var data = location.hash.slice(1);
-	if(data){
-		data=JSON.parse(decodeURIComponent(data.replace(/%28/g,"(").replace(/%29/g,")")));
-		fsh_e.setValue(unescape(data.fsh),-1);
-		if(data.size){w_.value = data.size[0];h_.value = data.size[1];}
-		if(data.fps)fps_.value = data.fps;
-	}
-	else if(localStorage.curdat){
-		data=JSON.parse(localStorage.curdat);
-		w_.value = data.size[0];h_.value = data.size[1];fps_.value = data.fps;
-		fsh_e.setValue(data.fsh,-1);
-		llog("<span style='color:#48f;'>loaded from cache</span>");
-	}
-};
-inpurl();save();
-const saveas=()=>{
-	var name=prompt();if(name==null)return;
-	curname=name||('untitled '+new Date().toLocaleString());
-	var dat=JSON.parse(localStorage.datli||'{}');
-	dat[curname]={thumb:curdat.thumb};console.log(dat);
-
-	//localStorage.datli=JSON.srringify(dat);
-},
-loadfrom=()=>{
-	//curdat=localStorage['dat_'+]
-};
+copy=x=>{navigator.clipboard.writeText(x).catch(e=>console.log(e));return x;};
 
 var c = document.getElementById("canvas"),gl = WebGL.setup(c,true,true);
 var prv = document.getElementById("preview"),prvctx = prv.getContext("2d");prvctx.imageSmoothingEnabled = false;
@@ -57,10 +20,8 @@ var prg=WebGL.compile(gl,vsh,'void main(){gl_FragColor=vec4(0);}');
 const resize=(c,prv,gl,w,h)=>{
 	c.width=w;prv.width=w;c.height=h;prv.height=h;
 	gl.viewport(0,0,gl.canvas.width,gl.canvas.height);
-}
-resize(c,prv,gl,w_.value,h_.value);
-
-const read=x=>{
+},
+read=x=>{
 	var url=URL.createObjectURL(x.files[0]);
 	document.getElementById(x.id+'d').style.backgroundImage=`url(${url})`;
 	tex(parseInt(x.id.slice(-1)),url);
@@ -107,4 +68,44 @@ const compile=()=>{
 	prg = WebGL.compile(gl,vsh,fsh_e.getValue());
 	llog(WebGL.log||'<span style="color:#6b4;">compile succeeded.</span>');
 }
-compile();
+
+
+
+const thumb=()=>{var prvtmp=[prv.width,prv.height];prv.width=32;prv.height=32;prvctx.drawImage(c,0,0,prv.width,prv.height);var s=prv.toDataURL();prv.width=prvtmp[0];prv.height=prvtmp[0];return s;},
+save=s=>{
+	curdat={
+		fsh:fsh_e.getValue(),
+		size:[w_.value,h_.value],
+		fps:fps_.value,
+		thumb:thumb()
+	}
+	localStorage.curdat=JSON.stringify(curdat);
+	if(s)llog("\n<span style='color:#48f;'>auto saved.</span>");else console.log("saved");
+},
+expurl=()=>'mcbeeringi.github.io/apps/webgl/fsh.html#'+encodeURIComponent(JSON.stringify(curdat)).replace(/\(/g,"%28").replace(/\)/g,"%29"),
+inpurl=()=>{
+	var data = location.hash.slice(1);
+	if(data){
+		data=JSON.parse(decodeURIComponent(data.replace(/%28/g,"(").replace(/%29/g,")")));
+		fsh_e.setValue(unescape(data.fsh),-1);
+		if(data.size){w_.value = data.size[0];h_.value = data.size[1];}
+		if(data.fps)fps_.value = data.fps;
+	}
+	else if(localStorage.curdat){
+		data=JSON.parse(localStorage.curdat);
+		w_.value = data.size[0];h_.value = data.size[1];fps_.value = data.fps;
+		fsh_e.setValue(data.fsh,-1);
+		llog("<span style='color:#48f;'>loaded from cache</span>");
+	}
+};
+inpurl();save();resize(c,prv,gl,w_.value,h_.value);compile();
+const saveas=()=>{
+	var name=prompt();if(name==null)return;
+	curname=name||('untitled '+new Date().toLocaleString());
+	var dat=JSON.parse(localStorage.datli||'{}');
+	dat[curname]={thumb:curdat.thumb};console.log(dat);
+	localStorage.datli=JSON.srringify(dat);
+},
+loadfrom=()=>{
+	//curdat=localStorage['dat_'+]
+};
