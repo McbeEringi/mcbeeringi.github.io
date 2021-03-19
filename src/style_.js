4bec5c36d2ead3767d44d82c51a73a2c5c34286f
McbeEringi <46889548+McbeEringi@users.noreply.github.com>
2021-03-19 23:13:26 +0900

    -

--- src/style_.js
+++ src/style_.js
@@ -6,9 +6,10 @@ a:link{color:#aef;}a:link:hover{color:#8af;}a:link:active{color:#48f;}
 a:visited{color:#caf;}a:visited:hover{color:#a8f;}a:visited:active{color:#84f;}
 @media(max-width:512px){li a{display:block;height:24px;padding:10px 0;margin:8px auto;background:#8884;}}
 #bg{position:fixed;top:0;left:0;width:100vw;height:100vh;object-fit:cover;z-index:-16;pointer-events:none;user-select:none;-webkit-user-select:none;}
-.stuff>a{display:inline-block;width:150px;margin:5px;vertical-align:top;text-align:center;transition:.2s;white-space:nowrap;}
+.stuff>a{display:inline-block;width:150px;margin:5px;vertical-align:top;text-align:center;transition:.2s;white-space:pre-wrap;}
 @media(max-width:504px){.stuff{text-align:center;}}
 .stuff>a::before{content:"";display:block;height:150px;background-size:100%;border-radius:20%;background-image:var(--img);background-color:var(--col);}
+.stuff>a::after{content:var(--txt);display:inline-block;font-size:small;text-decoration:none;color:#fff;width:100%;}
 .stuff>a:hover{transform:scale(1.05);filter:brightness(1.2)saturate(1.2)drop-shadow(0 0 8px #fea);}
 
 header{position:sticky;top:8px;left:0;width:100%;height:48px;background:#8888;line-height:48px;border-radius:8px;user-select:none;-webkit-user-select:none;backdrop-filter:blur(4px);-webkit-backdrop-filter:blur(4px);z-index:16;}
