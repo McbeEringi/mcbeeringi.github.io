bdb4d12b4d247506c7f97dd98f4edc77364683fc
McbeEringi <46889548+McbeEringi@users.noreply.github.com>
2021-11-21 15:28:00 +0900

    -

--- src/solo_style.js
+++ src/solo_style.js
@@ -1,15 +1,12 @@
 document.body.insertAdjacentHTML('beforeend',`<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/swiper/swiper-bundle.min.css">
 <style>
-	.swiper{width:90%;margin:0 auto;}
-	.swiper-slide{object-fit:contain;}
+	.swiper{width:90%;margin:0 auto;}.swiper-slide{object-fit:contain;}
 </style>`);
 fetch('https://cdn.jsdelivr.net/npm/swiper/swiper-bundle.min.js').then(x=>x.text()).then(x=>{
 	new Function(x)();
 	const swi=()=>new Swiper('.swiper',{
-		slidesPerView:1,spaceBetween:0,loop:true,centeredSlides:true,
-		breakpoints:{
-			600:{slidesPerView:2,spaceBetween:30}
-		},
+		slidesPerView:1,spaceBetween:30,loop:true,centeredSlides:true,
+		breakpoints:{600:{slidesPerView:2}},
 		autoplay:{delay:2500,disableOnInteraction:false,},
 		pagination:{el:'.swiper-pagination',clickable:true},
 		navigation:{nextEl:'.swiper-button-next',prevEl:'.swiper-button-prev'}
