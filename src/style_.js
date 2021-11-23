5262e486f2049fbb262a30ba1d20989f47ac5b96
McbeEringi <46889548+McbeEringi@users.noreply.github.com>
2021-11-23 23:47:41 +0900

    -

---
+++ src/style_.js
@@ -0,0 +1,12 @@
+'use strict';
+const _init=()=>{
+	document.body.insertAdjacentHTML('afterbegin',``);
+	document.body.insertAdjacentHTML('beforeend',`<hr>
+<footer>
+	2018~ @McbeEringi.｡:+*<br>
+	MIT License<br><br>
+	<a href="https://twitter.com/mcbeeringi"><img src="https://icongr.am/material/twitter.svg?color=ffffff" alt="twitter icon" width="32" height="32"></a>
+	<a href="https://github.com/mcbeeringi"><img src="https://icongr.am/material/github.svg?color=ffffff" alt="github icon" width="32" height="32"></a>
+</footer>`);
+};
+if(document.readyState=='loading')window.addEventListener('DOMContentLoaded',_init,{once:true});else _init();
