48076e614cceb68785ae58c8032f143213639b6a
McbeEringi <46889548+McbeEringi@users.noreply.github.com>
2021-11-24 13:24:48 +0900

    Update style_.js

--- src/style_.js
+++ src/style_.js
@@ -8,5 +8,6 @@ const _init=()=>{
 	<a href="https://twitter.com/mcbeeringi"><img src="https://icongr.am/material/twitter.svg?color=ffffff" alt="twitter icon" width="32" height="32"></a>
 	<a href="https://github.com/mcbeeringi"><img src="https://icongr.am/material/github.svg?color=ffffff" alt="github icon" width="32" height="32"></a>
 </footer>`);
+	document.querySelectorAll('a').forEach(e=>e.addEventListener('touchstart',()=>{},{passive:true}));
 };
 if(document.readyState=='loading')window.addEventListener('DOMContentLoaded',_init,{once:true});else _init();
