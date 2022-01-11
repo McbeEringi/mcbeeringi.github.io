b2aad4ff44af3901bf3e9ff1e606ebc29ea99737
McbeEringi <46889548+McbeEringi@users.noreply.github.com>
2022-01-11 20:34:10 +0900

    autosize→taheight

--- src/taheight.js
+++ src/taheight.js
@@ -1,1 +1,2 @@
-const taheight=x=>{Object.assign(x.style,{overflow:'hidden',wordWrap:'break-word',height:''});x.style.height=`${x.scrollHeight}px`;};
+const taheight=x=>{Object.assign(x.style,{overflow:'hidden',wordWrap:'break-word',height:''});x.style.height=`${x.scrollHeight}px`;},
+tainit=(...x)=>x.forEach(y=>{taheight(y);y.addEventListener('input',()=>taheight(y));});
