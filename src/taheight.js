af1afaa4da3a08f9c2b521ce8fb6820bbf261853
McbeEringi <46889548+McbeEringi@users.noreply.github.com>
2022-01-31 13:57:40 +0900

    Update taheight.js

--- src/taheight.js
+++ src/taheight.js
@@ -1,2 +1,2 @@
-const taheight=x=>{Object.assign(x.style,{boxSizing:'border-box',overflow:'hidden',height:''});x.style.height=`${x.offsetHeight+x.scrollHeight-x.clientHeight}px`;},
+const taheight=x=>Object.assign(x.style,{boxSizing:'border-box',overflow:'hidden',height:''}).height=x.offsetHeight+x.scrollHeight-x.clientHeight+'px',
 tainit=(...x)=>x.forEach(y=>{taheight(y);y.addEventListener('input',()=>taheight(y));});
