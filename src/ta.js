fad58499098b22383edaa530204c803156ca0f9d
McbeEringi <46889548+McbeEringi@users.noreply.github.com>
2022-01-31 16:49:04 +0900

    -

--- src/ta.js
+++ src/ta.js
@@ -1,14 +1,15 @@
 const TA={
-	brkt:['()','{}','[]'],quot:[...'\'"`'],
+	brkt:['()','{}','[]','「」','『』'],quot:[...'\'"`'],
 	ah:x=>Object.assign(x.style,{boxSizing:'border-box',overflow:'hidden',height:''}).height=x.offsetHeight+x.scrollHeight-x.clientHeight+'px',
 	sizer:x=>{TA.ah(x);x.addEventListener('input',()=>TA.ah(x));},
 	editor:x=>{TA.sizer(x);x.addEventListener('keydown',e=>{
-		const S=x.selectionStart,E=x.selectionEnd,cs=(y,z=(()=>{e.preventDefault();return y;})())=>{x.selectionStart=y;x.selectionEnd=z;},
-			ins=(y,z=1)=>{x.value=x.value.slice(0,S)+y+x.value.slice(E);cs(S+z);},del=()=>{if(S==E&&(TA.brkt.includes(x.value.slice(S-1,S+1))||(x.value[S-1]==x.value[S]&&TA.quot.includes(x.value[S]))))cs(S-1,E+1);};
+		if(e.isComposing)return;
+		const S=x.selectionStart,E=x.selectionEnd,pd=()=>e.preventDefault(),cs=(y,z=y)=>{x.selectionStart=y;x.selectionEnd=z;},
+			ins=y=>x.value=x.value.slice(0,S)+y+x.value.slice(E),del=()=>{if(S==E&&(TA.brkt.includes(x.value.slice(S-1,S+1))||(x.value[S-1]==x.value[S]&&TA.quot.includes(x.value[S]))))cs(S-1,E+1);};
 		(({
-			Tab:()=>ins('	'),Backspace:del,Delete:del,Enter:()=>{const s=x.value.slice(0,S).match(/([	 ]*).*$/)[1]+(TA.brkt.some(y=>x.value[S-1]==y[0])?'	':'');ins('\n'+s,s.length+1);},
-			...Object.fromEntries(TA.brkt.map(y=>[[y[0],()=>ins(y[0]+x.value.slice(S,E)+y[1])],[y[1],()=>{if(x.value[S]==y[1])cs(S+1);}]]).flat()),
-			...Object.fromEntries(TA.quot.map(y=>[y,()=>{if(x.value[S]==y)cs(S+1);else ins(y+x.value.slice(S,E)+y)}])),
+			Tab:()=>{pd();ins('	');cs(S+1);},Backspace:del,Delete:del,Enter:()=>{pd();const s='\n'+x.value.slice(0,S).match(/([	 ]*).*$/)[1]+(TA.brkt.some(y=>x.value[S-1]==y[0])?'	':'');ins(s);cs(S+s.length);},
+			...Object.fromEntries(TA.brkt.map(y=>[[y[0],()=>{ins(x.value.slice(S,E)+y[1]);cs(S);}],[y[1],()=>{if(x.value[S]==y[1]){pd();cs(S+1);}}]]).flat()),
+			...Object.fromEntries(TA.quot.map(y=>[y,()=>{if(x.value[S]==y){pd();cs(S+1);}if(x.value[S-1]!=y){ins(x.value.slice(S,E)+y);cs(S);}}])),
 		})[e.key]||Array)();
 		if(e.defaultPrevented)TA.ah(x);
 	});}
