901e813d8cc3e9750d11ce587940bdf5940cb08b
McbeEringi <46889548+McbeEringi@users.noreply.github.com>
2022-03-18 23:51:41 +0900

    -

--- src/style.js
+++ src/style.js
@@ -2,7 +2,7 @@
 const _init=()=>{
 	document.body.insertAdjacentHTML('afterbegin',`<style>
 	header{position:fixed;top:0;right:0;z-index:10;max-height:100%;display:flex;flex-direction:column;align-items:flex-end;}
-	header>input{appearance:none;-webkit-appearance:none;flex-shrink:0;display:block;width:48px;height:48px;margin:8px;border-radius:50%;background:0 0/100% url("https://mcbeeringi.github.io/img/icon.svg");box-shadow:0 0 4px var(--g0);}
+	header>input{appearance:none;-webkit-appearance:none;flex-shrink:0;display:block;width:48px;height:48px;margin:0;border-radius:50%;background:no-repeat center/70% url(https://icongr.am/material/menu.svg?color=ffffff);}
 	header>nav{transform:translateX(100%);visibility:hidden;transition:.2s;overflow:auto;}
 	header>nav>*{display:block;text-align:center;padding:8px 32px;margin:4px 8px;border-radius:8px;background:var(--g2) no-repeat center/contain;transform:scale(1);transition:inherit;text-decoration:none;font-weight:bold;text-decoration:none;backdrop-filter:blur(4px);-webkit-backdrop-filter:blur(4px);}
 	header>nav>*:hover{background-color:var(--g1);transform:scale(1.1);}
