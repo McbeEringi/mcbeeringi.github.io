3d34592a5ab345dc3a8173754b73afea424a0ad7
McbeEringi <46889548+McbeEringi@users.noreply.github.com>
2022-03-26 22:28:22 +0900

    -

--- src/gas.js
+++ src/gas.js
@@ -1,6 +1,8 @@
 (()=>{
-	const d=document,n=navigator,ls=localStorage,ss=sessionStorage,id=()=>Math.random().toString(36).slice(2),
-		send=x=>n.sendBeacon('https://script.google.com/macros/s/AKfycbxR1bd3vIjo3QHiyPaGmDs88j-n6T4zxPLmCI_Hsbjy15KZ1y7n-7pqLVpUGfv4WNyw/exec',JSON.stringify(Object.assign({cid:ls.cid||(ls.cid=id()),sid:ss.sid||(ss.sid=id()),t:Date.now(),loc:d.location.pathname+d.location.search},x)));
-	send({mobile:+('ontouchend'in d),ua:n.userAgent,scr:[screen.width,screen.height],dpr:devicePixelRatio,lang:n.language,ref:d.referrer});
-	['error','unhandledrejection','rejectionhandled'].forEach(y=>addEventListener(y,e=>send({err:y+': '+(e.reason||e.message)})));
+	const d=document,n=navigator,ls=localStorage,ss=sessionStorage,id=()=>Math.random().toString(36).slice(2),send=x=>n.sendBeacon('https://script.google.com/macros/s/AKfycbxR1bd3vIjo3QHiyPaGmDs88j-n6T4zxPLmCI_Hsbjy15KZ1y7n-7pqLVpUGfv4WNyw/exec',JSON.stringify(x));
+	send({lang:n.language,ref:d.referrer,loc:d.location.pathname+d.location.search});
+	['error','unhandledrejection','rejectionhandled'].forEach(y=>addEventListener(y,e=>send({
+		cid:ls.cid||(ls.cid=id()),t:Date.now(),loc:d.location.pathname+d.location.search,err:y+': '+(e.reason||e.message),
+		mobile:+('ontouchend'in d),ua:n.userAgent,scr:[screen.width,screen.height],dpr:devicePixelRatio
+	})));
 })();
