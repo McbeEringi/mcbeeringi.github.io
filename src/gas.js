629b269be905e0f08d5ce2e7c449d1969eba3f0c
McbeEringi <46889548+McbeEringi@users.noreply.github.com>
2022-03-26 11:34:28 +0900

    -

--- src/gas.js
+++ src/gas.js
@@ -1,6 +1,6 @@
-const d=document,n=navigator,ls=localStorage,ss=sessionStorage,id=()=>Math.random().toString(36);
+const d=document,n=navigator,ls=localStorage,ss=sessionStorage,id=()=>Math.random().toString(36).slice(2);
 n.sendBeacon('https://script.google.com/macros/s/AKfycbxR1bd3vIjo3QHiyPaGmDs88j-n6T4zxPLmCI_Hsbjy15KZ1y7n-7pqLVpUGfv4WNyw/exec',JSON.stringify({
 	cid:ls.cid||(ls.cid=id()),sid:ss.sid||(ss.sid=id()),
 	ua:n.userAgent,scr:[screen.width,screen.height],dpr:devicePixelRatio,lang:n.language,t:Date.now(),touch:'ontouchend'in d,
-	ref:d.referrer,page:[d.title,d.location.href],
+	ref:d.referrer,loc:d.location.origin+d.location.pathname
 }));
