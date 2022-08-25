c086ec4e83da2c394cd0ae551b853551b3c5dcc5
McbeEringi <46889548+McbeEringi@users.noreply.github.com>
2022-08-25 17:55:00 +0900

    -

--- src/style.js
+++ src/style.js
@@ -2,14 +2,14 @@
 const _init=()=>{
 	document.body.insertAdjacentHTML('afterbegin',`<style>
 	header{position:fixed;top:0;right:0;z-index:10;max-height:100%;display:flex;flex-direction:column;align-items:flex-end;}
-	header>input{appearance:none;-webkit-appearance:none;flex-shrink:0;display:block;width:48px;height:48px;margin:0;border-radius:50%;background:no-repeat center/70% url(https://icongr.am/feather/menu.svg?color=ffffff);}
+	header>button{appearance:none;-webkit-appearance:none;flex-shrink:0;display:block;width:48px;height:48px;margin:0;padding:0;border:0;border-radius:50%;background:no-repeat center/70% url(https://icongr.am/feather/menu.svg?color=ffffff);}
 	header>nav{transform:translateX(100%);visibility:hidden;transition:.2s;overflow:auto;}
 	header>nav>*{display:block;text-align:center;padding:8px 32px;margin:4px 8px;border-radius:8px;background:var(--g2) no-repeat center/contain;transform:scale(1);transition:inherit;text-decoration:none;font-weight:bold;text-decoration:none;backdrop-filter:blur(4px);-webkit-backdrop-filter:blur(4px);}
 	header>nav>*:hover{background-color:var(--g1);transform:scale(1.1);}
-	header>input:checked+nav{transform:translateX(0);visibility:visible;}
+	header>button:hover+nav,header>button:focus+nav,nav:hover,nav:focus-within{transform:translateX(0);visibility:visible;}
 	.lang{background-image:url(https://icongr.am/feather/globe.svg?color=ffffff);}
 </style>
-<header><input type="checkbox"><nav>
+<header><button></button><nav>
 	<a href="/">Top</a>
 	<a href="https://mcbeeringi.github.io/sky/">Sky</a>
 	<a href="https://mcbeeringi.github.io/apps/">Apps</a>
@@ -20,7 +20,7 @@ const _init=()=>{
 </nav></header>`);
 	document.body.insertAdjacentHTML('beforeend',`<hr><footer>2018~ @McbeEringi.｡:+*<br>MIT License</footer>`);
 	document.querySelectorAll('a.lang').forEach(e=>e.href='https://translate.google.com/translate?sl=ja&tl=en&u='+encodeURIComponent(location.href));
-	document.querySelectorAll('a').forEach(e=>e.addEventListener('touchstart',()=>{},{passive:true}));
-	document.documentElement.setAttribute('lang',window.navigator.language.slice(0,2));
+	document.documentElement.addEventListener('touchstart',_=>_,{passive:true});
+	document.documentElement.setAttribute('lang',navigator.language.slice(0,2));
 };
-if(document.readyState=='loading')window.addEventListener('DOMContentLoaded',_init,{once:true});else _init();
+if(document.readyState=='loading')addEventListener('DOMContentLoaded',_init,{once:true});else _init();
