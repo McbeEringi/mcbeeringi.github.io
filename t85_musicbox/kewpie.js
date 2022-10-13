ccc9e3243b0dad8fc69deb6e04bdb8c30cb700dd
McbeEringi <46889548+McbeEringi@users.noreply.github.com>
2022-10-14 00:02:48 +0900

    -

---
+++ t85_musicbox/kewpie.js
@@ -0,0 +1,39 @@
+({
+	bpm:168,
+	minNote:1/16,
+	vwd:[
+		0b010,
+		0b010,
+		0b010,
+		0b010
+	],
+	notes:`
+		2r>1gg2ggee4f2r1gg2ggCC4d2r1gg8gd<g>4r2g1gg
+		2ergrgrr1gg2frgrgrr1gg2ergrbrargFfedrr1gg
+		2ergrgrr1gg2drbrbrr1aa2arar>drd<r4g1gGaA2bgag
+		ergregrgfrgrfgrgergrbrargFfedrr1gg
+		2ergrgrr1gg2frarar>crer4r2dr4r
+		2c<1gGaA2b>c<<r4c,
+
+		rr>2CC4drr<2AA4br2r>8Fc2r8g4r<<
+		2rgrgrgrgrgrgrgrgrgrgrgrgrgrgrgrg
+		rgrgrgrgrbrbrbrbrararara4gr8r
+		2rgrgrgrgrgrgrgrgrgrgrgrgrgrgrgrg
+		rgrgrgrgrarararargrgrgrg
+		gr4r>2cr<4c,
+
+		r16rr2rr>8f<b4r8g<
+		2rererererfrfrfrfrererererfrfrfrf
+		rerererergrgrgrgrFrFrFrF16r
+		2rererererfrfrfrfrererererfrfrfrf
+		rererererfrfrfrfrerdrfrf
+		er4r2rr<4c,
+
+		r16rr2rrr>8e<a2r8r
+		<4c<gcgdgdg>c<gcgdgdg
+		>c<gcgdgdgadFdgr8r
+		>4c<g>cc<gdgg>c<g>cc<gdgg
+		>c<g>cc<fcff>c<gdg
+		>2cr4rrr
+	`
+})
---
+++ t85_musicbox/small_world.js
@@ -0,0 +1,55 @@
+({
+	bpm:130,
+	minNote:1/16,
+	vwd:[
+			0b010,
+			0b110,
+			0b100,
+			0b000
+	],
+	notes:`
+		2b>c
+		4dbg2ag4gFF<2ab>
+		4caF2gF4edd<2b>c
+		4d2ga4b2ag4e2ab>4c<2ba
+		4d>c<ba8gr
+
+		6g2g4bg6a2a8a
+		6a2a4>c<a6b2b8b
+		6b2b4>d<b>6c2c4c2<ba
+		8dFg4r,
+
+		r2<
+		rbrbrbrb>rcrcrcrc
+		rcrcrcrc<rbrbrbrb
+		rbrb>rdrdrererere
+		rdrdrcrc<rbrbbrr>c<
+
+		rbrbrbrb>rcrcrcrc
+		rcrcrcrc<rbrbrbrb
+		rbrb>rdrdrererere
+		rdrdrcrc<rbrbbr,
+
+		r2<
+		rgrgrgrgrararara
+		rarararargrgrgrg
+		rgrgrbrb>rcrcrCrC
+		rcrc<rarargrggrra
+
+		rgrgrgrgrararara
+		rarararargrgrgrg
+		rgrgrbrb>rcrcrCrC
+		rcrc<rarargrggr,
+
+		r<<
+		gdgdadad
+		adadgdgd
+		ggbb>ccCC
+		d<dFagdgd
+
+		gdgdadad
+		adadgdgd
+		ggbb>ccCC
+		d<dFagdg
+	`
+})
