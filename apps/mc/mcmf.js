aab0dca59ef8b7f753ff6300dae9a72df17dc975
McbeEringi <46889548+McbeEringi@users.noreply.github.com>
2022-10-28 00:01:15 +0900

    -

--- apps/mc/mcmf.js
+++ apps/mc/mcmf.js
@@ -1,16 +1,7 @@
-//https://qiita.com/psn/items/d7ac5bdb5b5633bae165
-const uuidgen=()=>'xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx'.split('').map(e=>{switch(e){case'x':return Math.floor(Math.random()*16).toString(16);case'y':return (Math.floor(Math.random()*4)+8).toString(16);default:return e;}}).join('');
-const simple_mcmf=x=>{
-	var r={},h={},m={},tmp={};
-	switch(typeof x){
-		case'string':x.split('; ').map(c=>tmp[c.substr(0,1)]=c.substr(1));tmp.v=JSON.parse(tmp.v);if(tmp.m)tmp.m=JSON.parse(tmp.m);break;
-		case'object':break;
-	}
-	console.log(tmp)
-	r.format_version=1;
-	h.name=tmp.n;h.description=tmp.d;h.version=tmp.v;
-	if(tmp.m){h.min_engine_version=tmp.m;if(Number(tmp.m.join(''))>=1130)r.format_version=2;}
-	m.type=tmp.t;m.version=tmp.v;
-	h.uuid=uuidgen();m.uuid=uuidgen();r.header=h;r.modules=[m];
-	return JSON.stringify(r);
-}
+const
+uuidgen=self.crypto&&self.crypto.randomUUID?(_=>self.crypto.randomUUID()):(_=>Array.from('xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx',x=>({x:_=>(Math.random()*16|0).toString(16),y:_=>'89ab'[Math.random()*4|0]}[x]||(_=>x))()).join('')),
+mcmf=({n:name='',d:description='',t:type='resources',v:version=[0,0,1],m}={})=>JSON.stringify({
+	format_version:m&&+m.join('')>=1130?2:1,
+	header:{name,description,version,...(m?{min_engine_version:m}:{}),uuid:uuidgen()},
+	modules:[{type,version,uuid:uuidgen()}]
+});
