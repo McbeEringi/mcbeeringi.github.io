8a07f91b60960634cf971935f28550621e50f09e
McbeEringi <46889548+McbeEringi@users.noreply.github.com>
2022-11-01 01:31:14 +0900

    -

--- apps/mc/mcmf.js
+++ apps/mc/mcmf.js
@@ -1,7 +1,3 @@
 const
-uuidgen=self.crypto&&self.crypto.randomUUID?(_=>self.crypto.randomUUID()):(_=>Array.from('xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx',x=>({x:_=>(Math.random()*16|0).toString(16),y:_=>'89ab'[Math.random()*4|0]}[x]||(_=>x))()).join('')),
-mcmf=({n:name='',d:description='',t:type='resources',v:version=[0,0,1],m}={})=>JSON.stringify({
-	format_version:m&&+m.join('')>=1130?2:1,
-	header:{name,description,version,...(m?{min_engine_version:m}:{}),uuid:uuidgen()},
-	modules:[{type,version,uuid:uuidgen()}]
-});
+uuidgen=self.crypto.randomUUID?(_=>self.crypto.randomUUID()):(w=>(w=[...self.crypto.getRandomValues(new Uint8Array(16))].flatMap(x=>[x>>4,x&0xf]),Array.from('00000000-0000-4000-1000-000000000000',x=>([_=>w.pop().toString(16),_=>'89ab'[w.pop()%4]][x]||(_=>x))()).join(''))),
+mcmf=({n:name='',d:description='',t:type='resources',v:version=[0,0,1],m}={})=>JSON.stringify({format_version:m&&+m.join('')>=1130?2:1,header:{name,description,version,...(m?{min_engine_version:m}:{}),uuid:uuidgen()},modules:[{type,version,uuid:uuidgen()}]});
