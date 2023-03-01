789760d4ae8a9aa61c075007fa19628990ff0045
McbeEringi <46889548+McbeEringi@users.noreply.github.com>
2023-03-02 03:40:48 +0900

    -

--- src/style.js
+++ src/style.js
@@ -3,9 +3,9 @@ self.root=document.querySelector('script[src$="src/style.js"]').outerHTML.match(
 (w=>document.readyState=='loading'?addEventListener('DOMContentLoaded',w,{once:true}):w())((_,a='</a><a class="button" href="')=>((w=>(
 	w.insertAdjacentHTML('afterbegin',`<style>
 		header{position:-webkit-sticky;position:sticky;top:8px;z-index:10;pointer-events:none;}
-		header>.button{font-size:large;margin:0;pointer-events:auto;}header>.button::before{content:"";display:inline-block;height:4ex;width:4ex;margin-right:var(--border);border-radius:50%;vertical-align:middle;background:0 0/100% url(${root}img/icon.svg);}
-		header>.input{position:absolute;width:max(32ex,33%);max-height:calc(calc(100vh - 100%) - 16px);overflow:auto;transform:translateX(-5%);transition:.2s;opacity:0;visibility:hidden;pointer-events:none;display:grid;grid-template-columns:repeat(auto-fill,minmax(min(12ex,100%),1fr));gap:var(--border);}header>.input>*{text-align:center;margin:0;}
-		header>button:hover+nav,header>button:focus+nav,nav:hover,nav:focus-within{top:unset;transform:translateY(0);opacity:1;visibility:visible;pointer-events:auto;}
+		header>.button{font-size:large;margin:0;pointer-events:auto;}header>.button::before{content:"";display:inline-block;height:4ex;width:4ex;margin-right:var(--bor);border-radius:50%;vertical-align:middle;background:0 0/100% url(${root}img/icon.svg);}
+		header>.input{position:absolute;width:max(32ex,33%);max-height:calc(calc(100vh - 100%) - 16px);overflow:auto;transform:translateX(-5%);transition:.2s;opacity:0;visibility:hidden;pointer-events:none;display:grid;grid-template-columns:repeat(auto-fill,minmax(min(12ex,100%),1fr));gap:var(--bor);}header>.input>*{text-align:center;margin:0;}
+		header>button:hover+nav,header>button:focus+nav,nav:hover,nav:focus-within{transform:none;opacity:1;visibility:visible;pointer-events:auto;}
 	</style><header><button class="button">McbeEringi</button><nav class="input">`+`
 	,Top;sky/,Sky Stuff;apps/,Apps;ta/,TA.js;petit/,PetitJS;mc/,Minecraft;
 	translate.google.com/translate?sl=ja&tl=en&u=${encodeURIComponent(location)},Translate;twitter.com/mcbeeringi,Twitter;github.com/mcbeeringi,GitHub;qiita.com/mcbeeringi,Qiita;youtube.com/@mcbeeringi,YouTube;
