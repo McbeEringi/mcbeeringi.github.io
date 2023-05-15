6b7b8d10771b29e592d936b2cf09c1b9da7e800e
McbeEringi <46889548+McbeEringi@users.noreply.github.com>
2023-05-15 19:42:52 +0900

    -

--- src/cm/cm.bat
+++
@@ -1,7 +0,0 @@
-//&cls&node %0
-const wf=require('fs').promises.writeFile,p='https://codemirror.net/try/mods/';
-console.log('CodeMirror Downloader\nSearching...');
-(async _=>(_=(await(await fetch(p)).text()).match(/href=".*?\.js"/g),console.log(_.length+' files found.'),_.forEach(async x=>(
-	x=decodeURIComponent(x.slice(6,-1)),
-	wf(x,(await(await fetch(p+x)).blob()).stream()),console.log(x)
-))))()
---
+++ src/cm/main.bat
@@ -0,0 +1,8 @@
+//&cls&node %0
+const wf=require('fs').promises.writeFile,p='https://codemirror.net/try/mods/';
+(async _=>(
+	console.log('CodeMirror Downloader\nSearching...'),wf('index.html',_=await(await fetch(p)).text()),
+	console.log(_.match(/href=".*?\.js"/g).map(async x=>(
+		wf(x=decodeURIComponent(x.slice(6,-1)),(await(await fetch(p+x)).blob()).stream()),console.log(x)
+	)).length+' files found.')
+))()
