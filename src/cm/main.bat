2168a91b42f7f024b04168ae279c1765f7fcf177
McbeEringi <46889548+McbeEringi@users.noreply.github.com>
2023-05-15 19:58:03 +0900

    -

--- src/cm/main.bat
+++ src/cm/main.bat
@@ -1,4 +1,9 @@
-//&cls&node %0
+//&cls&node %0&exit
+/*
+	CodeMirror https://github.com/codemirror/dev/blob/main/LICENSE
+		Copyright (C) 2018 by Marijn Haverbeke <marijn@haverbeke.berlin>, AdrianHeine <mail@adrianheine.de>, and others
+		Distributed under MITLicense
+*/
 const wf=require('fs').promises.writeFile,p='https://codemirror.net/try/mods/';
 (async _=>(
 	console.log('CodeMirror Downloader\nSearching...'),wf('index.html',_=await(await fetch(p)).text()),
