ae44482fde849341c7af832ae0e078d68311bde6
McbeEringi <46889548+McbeEringi@users.noreply.github.com>
2023-09-05 20:16:02 +0900

    -

--- src/gas.js
+++ src/gas.js
@@ -1,5 +1,5 @@
 'use strict';
-(w=>d.readyState=='loading'?addEventListener('DOMContentLoaded',w,{once:true}):w())(_=>{
+(w=>document.readyState=='loading'?addEventListener('DOMContentLoaded',w,{once:true}):w())(_=>{
 	const d=document,n=navigator,ls=localStorage,ss=sessionStorage,id=()=>Math.random().toString(36).slice(2),send=x=>n.sendBeacon('https://script.google.com/macros/s/AKfycbxR1bd3vIjo3QHiyPaGmDs88j-n6T4zxPLmCI_Hsbjy15KZ1y7n-7pqLVpUGfv4WNyw/exec',JSON.stringify(x));
 	send({lang:n.language,ref:d.referrer,loc:d.location.pathname+d.location.search});
 	['error','unhandledrejection','rejectionhandled'].forEach(y=>addEventListener(y,e=>send({
