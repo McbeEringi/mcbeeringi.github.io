1725780b5a276eeac3630526c58b373f55bcfe13
McbeEringi <46889548+McbeEringi@users.noreply.github.com>
2023-12-13 02:46:49 +0900

    -

--- src/cm/main.bat
+++ src/cm/main.bat
@@ -4,6 +4,6 @@ const p='https://codemirror.net/try/mods/';
 (async _=>(
 	console.log('CodeMirror Downloader\nSearching...'),wf('cm-index.html',_=await(await fetch(p)).text()),
 	console.log(_.match(/href=".*?\.js"/g).map(async x=>(
-		wf(x=decodeURIComponent(x.slice(6,-1)),await(await fetch(p+x)).blob()).stream(),console.log(x)
+		wf(x=decodeURIComponent(x.slice(6,-1)),(await(await fetch(p+x)).blob()).stream()),console.log(x)
 	)).length+' files found.')
 ))();
