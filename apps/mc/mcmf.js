ca33b76c164e2e774e3028848d4486f82e8e52d8
McbeEringi <git@mcbeeringi.dev>
2024-11-12 19:29:53 +0900

    -

--- apps/mc/mcmf.js
+++ apps/mc/mcmf.js
@@ -1,2 +1,3 @@
 self.uuidgen=self.crypto.randomUUID?(_=>self.crypto.randomUUID()):(w=>(w=[...self.crypto.getRandomValues(new Uint8Array(16))].flatMap(x=>[x>>4,x&0xf]),Array.from('00000000-0000-4000-1000-000000000000',x=>([_=>w.pop().toString(16),_=>'89ab'[w.pop()%4]][x]||(_=>x))()).join('')));
+// '00000000-0000-4000-1000-000000000000'.replace(/[01]/g,x=>(y=>+x?y*4|8:y*16|0)(Math.random()).toString(16))
 self.mcmf=({n='',d='',t='resources',v=[0,0,1],m=[1,13,0]}={})=>JSON.stringify({format_version:2,header:{name:n,description:d,version:v,min_engine_version:m,uuid:uuidgen()},modules:[{type:t,version:v,uuid:uuidgen()}]});
